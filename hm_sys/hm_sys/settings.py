d5e2d75eaa963d66490e7f512af7f3806763324a
gxrhariagasthya <gxrhariagasthya@gmail.com>
2025-04-24 15:02:26 +0530

    runned template to dvp_br

--- hm_sys/hm_sys/settings.py
+++ hm_sys/hm_sys/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home_app",
 ]
 
 MIDDLEWARE = [
@@ -110,12 +111,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS=[BASE_DIR/"home_app/static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
